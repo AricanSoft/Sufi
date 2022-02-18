d22286f36cf2dfe76a727b182d621e39b18f4376
AricanSoft <89875962+AricanSoft@users.noreply.github.com>
2022-02-18 19:02:54 +0300

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// Assembly Sufi Web Tarayıcısı, Version 1.0.0.0
+
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.DisableOptimizations | System.Diagnostics.DebuggableAttribute.DebuggingModes.EnableEditAndContinue | System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | System.Diagnostics.DebuggableAttribute.DebuggingModes.Default)]
+[assembly: System.Reflection.AssemblyTitle("Sufi Web Tarayıcısı")]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCompany("Void INC.")]
+[assembly: System.Reflection.AssemblyProduct("Sufi Web Tarayıcısı")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 TheMarqE 2020")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("e17bbe88-0dee-419f-8543-14442ad0762c")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Runtime.Versioning.TargetFramework(".NETFramework,Version=v4.5.2", FrameworkDisplayName=".NET Framework 4.5.2")]
+
---
+++ Bunifu_Browser/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Bunifu_Browser
+{
+    using EasyTabs;
+    using System;
+    using System.Windows.Forms;
+
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                AppContainer parent = new AppContainer();
+                TitleBarTab item = new TitleBarTab(parent);
+                frmBrowser browser1 = new frmBrowser {
+                    Text = "Sufi - Yeni Sekme"
+                };
+                item.Content = browser1;
+                parent.Tabs.Add(item);
+                parent.SelectedTabIndex = 0;
+                TitleBarTabsApplicationContext context = new TitleBarTabsApplicationContext();
+                context.Start(parent);
+                Application.Run(context);
+            }
+            catch
+            {
+                MessageBox.Show("Sufi Bir Sorunla Karşılaştı!", "Sufi");
+                Environment.Exit(1);
+            }
+        }
+    }
+}
+
